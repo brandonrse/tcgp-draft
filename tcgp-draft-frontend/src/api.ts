af61e08d986bd7fc514cae8db7b09642d7635b99
Brandon R <46826762+brandonrse@users.noreply.github.com>
2025-05-16 21:21:57 -0400

    Update api.ts

--- tcgp-draft-frontend/src/api.ts
+++ tcgp-draft-frontend/src/api.ts
@@ -1,5 +1,6 @@
 const API_BASE = process.env.VITE_API_URL || '';
 
 export function apiFetch(path: string, options?: RequestInit) {
+  console.log(API_BASE);
   return fetch(`${API_BASE}${path}`, options);
 }
