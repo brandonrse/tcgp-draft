ea46ccf4e4f6cf5a0c11558334cede74a5a907c3
Brandon R <46826762+brandonrse@users.noreply.github.com>
2025-05-28 12:47:17 -0400

    Update server.js

--- tcgp-draft-server/server.js
+++ tcgp-draft-server/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const cors = require('cors');
 const { Server } = require('socket.io');
 const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
 const path = require('path');
 
 const app = express();
